d59ac381d86528b9b29e2538af4202de87aba572
Geetika Pant <65403760+geetikapant@users.noreply.github.com>
2020-07-18 23:12:03 +0530

    Add files via upload

---
+++ array_introduction.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  int a[1000];
+  int i,n;
+  cin>>n;
+  for(i=0;i<n;i++)
+  {
+      cin>>a[i];
+
+  }
+  for(i=n-1;i>=0;i--)
+  {
+      cout<<a[i]<<" ";
+  }
+    return 0;
+}
+

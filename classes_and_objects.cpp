729d02f9d49d00029b968cc6cb6dd80c02622825
Geetika Pant <65403760+geetikapant@users.noreply.github.com>
2020-07-20 23:52:49 +0530

    Add files via upload

---
+++ classes_and_objects.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+    private:
+    int sum;
+    int arr[5];
+    public:
+    void input()
+    {
+        for(int i=0;i<5;i++)
+        {
+            cin>>arr[i];
+        }
+    }
+    int calculateTotalScore()
+    {
+        sum=0;
+         for(int i=0;i<5;i++)
+         {
+             sum=sum+arr[i];
+             
+         }
+         return sum;
+    }
+};
+int main() {
+    int n; 
+    cin >> n;
+    Student *s = new Student[n]; 
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+    int kristen_score = s[0].calculateTotalScore();
+
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    
+    cout << count;
+    
+    return 0;
+}
+
+
+
